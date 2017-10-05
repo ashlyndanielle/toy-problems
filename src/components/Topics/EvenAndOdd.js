2d7b1dd0470452b110b5aec03a9fd835ac176061
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-05 15:41:46 -0600

    set up project

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EvenAndOdd extends Component {
+  render() {
+    return (
+      <div>
+        Even and Odd
+      </div>
+    );
+  }
+}
+
+export default EvenAndOdd;
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilterObject extends Component {
+  render() {
+    return (
+      <div>
+        Filter Object
+      </div>
+    );
+  }
+}
+
+export default FilterObject;
