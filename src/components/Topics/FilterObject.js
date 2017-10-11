d034c78cfeac24cc7b6f7d76080232f1700c46ec
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-10 19:43:04 -0600

    filter object component started

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
 class FilterObject extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      unfilteredArray: [
+        {
+          color: 'black',
+          size: 'small',
+          hair: 'long'
+        },
+        {
+          color: 'purple',
+          age: 20,
+          wage: 30,
+        },
+        {
+          age: 34,
+          name: 'Stephanie',
+          shoe: 'large'
+        }
+      ],
+      userInput: '',
+      filteredArray: []
+    }
+  }
+
+  handleInput = value => {
+    this.setState({
+      userInput: value
+    })
+  }
+
   render() {
     return (
-      <div>
-        Filter Object
+      <div className="puzzleBox filterObjectPB">
+        <h4>Filter Object</h4>
+        <span className="puzzleText"></span>
+        <input 
+          className="inputLine"
+          onChange={ e => this.handleInput(e.target.value) }
+          value={this.state.userInput}
+        />
+        <button className="confirmationButton"></button>
+        <span className="resultsBox filterObjectRB"></span>
       </div>
     );
   }
