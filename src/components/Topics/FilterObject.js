4cfdba86876f3a1162689ef39db854d74f2bc3c9
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-13 10:53:42 -0600

    filtered object component almost finished

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -31,19 +31,20 @@ class FilterObject extends Component {
       userInput: value
     })
   }
+  filterThings = input => {}
 
   render() {
     return (
       <div className="puzzleBox filterObjectPB">
         <h4>Filter Object</h4>
-        <span className="puzzleText"></span>
+        <span className="puzzleText">{ JSON.stringify(this.state.unfilteredArray, null, 10) }</span>
         <input 
           className="inputLine"
           onChange={ e => this.handleInput(e.target.value) }
           value={this.state.userInput}
         />
-        <button className="confirmationButton"></button>
-        <span className="resultsBox filterObjectRB"></span>
+        <button className="confirmationButton" onClick={ this.filterThings(this.state.userInput) } >Filter</button>
+        <span className="resultsBox filterObjectRB">{ this.state.filteredArray }</span>
       </div>
     );
   }
