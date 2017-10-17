f8bc7ee4499d23413bf247a6fd9c491cca7742b5
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-10-17 11:24:01 -0600

    palindrome component all set up AND saved

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -3,14 +3,28 @@ import React, { Component } from 'react';
 class Palindrome extends Component {
   constructor() {
     super();
+    this.state = {
+      userInput: '',
+      palindrome: ''
+    }
+  }
+
+  handleChange = value => {
+    this.setState({
+      userInput: value
+    })
+  }
+
+  checkForPalindrome = input => {
     
   }
+
   render() {
     return (
       <div className="puzzleBox filterStringPB">
         <h4>Palindrome</h4>
-        <input className="inputLine" onChange={  } />
-        <button className="confirmationButton" onClick={  } ></button>
+        <input className="inputLine" onChange={ e => this.handleChange(e.target.value) } />
+        <button className="confirmationButton" onClick={ () => this.checkForPalindrome(this.state.userInput) } ></button>
         <span className="resultsBox"></span>
       </div>
     );
